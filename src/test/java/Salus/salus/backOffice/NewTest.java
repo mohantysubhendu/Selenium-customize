b482288ebbf6d0d0154fc72fa7a2eddd65360c7f
mohantysubhendu <38078924+mohantysubhendu@users.noreply.github.com>
2018-09-24 14:12:33 +0530

    Update NewTest.java

--- src/test/java/Salus/salus/backOffice/NewTest.java
+++ src/test/java/Salus/salus/backOffice/NewTest.java
@@ -55,8 +55,8 @@ public class NewTest {
 	
 	  @BeforeClass
 	  public void beforeClass() {
-		System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
-		//  System.setProperty("webdriver.chrome.driver","/Users/Shared/Jenkins/Home/chromedriver");
+		//System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+		  System.setProperty("webdriver.chrome.driver","/Users/Shared/Jenkins/Home/chromedriver");
 		 driver = new ChromeDriver();
 	  }
 
